9e1849eb2ad1f2818eafcd49a0500db06cabdc86
Viethiep49 <truongviethiep49@gmail.com>
2026-02-16 17:59:55 +0700

    buoi3

---
+++ 2380600637_TruongVietHiep_buoi3/2380600637_TruongVietHiep_buoi3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2380600637_TruongVietHiep_buoi3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
